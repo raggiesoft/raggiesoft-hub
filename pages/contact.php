b87944454d841d8329a36c855e2357ab5d804027
Michael Ragsdale <michael.ragsdale@raggiesoft.com>
2025-11-29 19:38:21 -0500

    Getting started with KNOX

---
+++ pages/contact.php
@@ -0,0 +1,10 @@
+<div class="container py-5">
+    <h1 class="display-5 fw-bold border-bottom pb-2 mb-4">Contact Me</h1>
+
+    <div class="fs-5"> <p class="lead mb-4">I can be reached at michael.ragsdale@raggiesoft.com</p>
+    <p>We could have a street address here if this was a business</p>
+    <p>1234 Anystreet.<br />
+    Unit #521<br />
+    Anytown, ST 12345</p>
+    <p>Phone: 1-800-555-1234</p>
+</div>
