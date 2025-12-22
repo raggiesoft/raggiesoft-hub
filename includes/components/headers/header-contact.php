f13d6f42a6eebe50512686213b535c01555e5d99
Michael Ragsdale <michael.ragsdale@raggiesoft.com>
2025-12-21 19:00:03 -0500

    WIP: Automated sync commit

--- includes/components/headers/header-contact.php
+++ includes/components/headers/header-contact.php
@@ -10,26 +10,31 @@ $request_uri = $_SERVER['REQUEST_URI'] ?? '/';
   
   <li class="nav-item">
     <a class="nav-link" href="/">
-        <i class="fa-duotone fa-house me-2"></i>Home
+        <i class="fa-duotone fa-house me-2" aria-hidden="true"></i>Home
     </a>
   </li>
 
   <li class="nav-item">
     <a class="nav-link" href="/about/michael-ragsdale">
-        <i class="fa-duotone fa-user-visor me-2"></i>The Architect
+        <i class="fa-duotone fa-user-visor me-2" aria-hidden="true"></i>The Architect
     </a>
   </li>
 
   <li class="nav-item">
     <a class="nav-link" href="/engine-room">
-        <i class="fa-solid fa-industry me-2"></i>Engine Room
+        <i class="fa-solid fa-industry me-2" aria-hidden="true"></i>Engine Room
     </a>
   </li>
 
   <li class="nav-item">
-    <a class="nav-link active" href="/contact">
-        <i class="fa-duotone fa-envelope-open me-2"></i>Contact
+    <a class="nav-link" href="/contact">
+        <i class="fa-duotone fa-envelope-open me-2" aria-hidden="true"></i>Contact
     </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="/about">
+        <i class="fa-duotone fa-circle-info me-2" aria-hidden="true"></i>About 
   </li>
 
 </ul>
