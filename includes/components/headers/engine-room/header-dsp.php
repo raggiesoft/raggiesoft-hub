0fd012806742015c17a881333f4a1f8fb06f38a3
Michael Ragsdale <michael.ragsdale@raggiesoft.com>
2026-06-04 23:16:55 -0400

    Engine Room Records Corporate is coming online

--- includes/components/headers/engine-room/header-dsp.php
+++ includes/components/headers/engine-room/header-dsp.php
@@ -5,14 +5,14 @@
 <ul class="navbar-nav ms-auto mb-2 mb-md-0">
   
   <li class="nav-item">
-    <a class="nav-link active fw-bold text-dark" href="/engine-room/dsp-verification">
-        <i class="fa-solid fa-folder-open me-2"></i>Master Directory
+    <a class="nav-link active fw-bold text-info" aria-current="page" href="/engine-room/dsp-verification">
+        <i class="fa-solid fa-folder-open me-2" aria-hidden="true"></i>Master Directory
     </a>
   </li>
 
-  <li class="nav-item border-start ms-2 ps-2">
-      <a class="nav-link text-body-secondary hover-text-primary" href="mailto:dsp.operations@engineroom-records.com">
-        <i class="fa-solid fa-envelope me-2"></i><span class="small">Contact DSP Support</span>
+  <li class="nav-item border-start border-secondary ms-2 ps-2">
+      <a class="nav-link text-body-secondary hover-text-info" href="mailto:dsp.operations@engineroom-records.com">
+        <i class="fa-solid fa-envelope me-2" aria-hidden="true"></i><span class="small">Contact DSP Support</span>
       </a>
   </li>
 
