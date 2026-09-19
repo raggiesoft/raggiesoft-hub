c970ed6471b8c185664481b81c59920a9bff216d
Michael Ragsdale <michael.ragsdale@raggiesoft.com>
2026-09-19 14:17:22 -0400

    Web Awesome Deployment Test

--- pages/engine-room/artists/stardust-engine/discography/overview.php
+++ pages/engine-room/artists/stardust-engine/discography/overview.php
@@ -81,7 +81,7 @@ $musicGroupSchema = [
                 </div>
             </div>
 
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 g-4">
                 <?php foreach ($eraData['albums'] as $album): 
                     // Metadata Extraction
                     $isSeized = (isset($album['extra']) && str_contains($album['extra'], 'CANCELED'));
